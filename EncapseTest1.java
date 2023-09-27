3593edc229c10cad9e99851c371a23c6bce0e2b0
unknown <you@example.com>
2023-09-27 09:40:13 +0530

    encapsulation

---
+++ EncapseTest1.java
@@ -0,0 +1,18 @@
+package encapsulation;
+
+public class EncapseTest1 {
+	
+	    public static void main(String args[]){
+	         EncapsulationDemo obj = new EncapsulationDemo();
+	         obj.setEmpName("chandana");
+	         obj.setEmpAge(23);
+	         obj.setEmpSSN(112233);
+	         System.out.println("Employee Name: " + obj.getEmpName());
+	         System.out.println("Employee SSN: " + obj.getEmpSSN());
+	         System.out.println("Employee Age: " + obj.getEmpAge());
+	    } 
+	}
+
+
+	
+		// TODO Auto-generated method stub
